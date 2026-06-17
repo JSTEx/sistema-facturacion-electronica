767f3091ef55ca6608c848d6fe5b77891bdc52a1
JSTEx <ghostblack5034@gmail.com>
2026-06-17 01:42:59 -0600

    refactor: unificar configuracion de Firebase y correcciones

---
+++ assets/js/core/firebase-config.js
@@ -0,0 +1,11 @@
+// Configuración centralizada de Firebase - compartida entre todas las páginas
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyB3spppto5IEYRAxFOe0jdBGAnMvJRn8K8",
+    authDomain: "facturacionelectronica-c2155.firebaseapp.com",
+    databaseURL: "https://facturacionelectronica-c2155-default-rtdb.firebaseio.com",
+    projectId: "facturacionelectronica-c2155",
+    storageBucket: "facturacionelectronica-c2155.firebasestorage.app",
+    messagingSenderId: "1071386903920",
+    appId: "1:1071386903920:web:c58b1a0148d3856b5a3bb5",
+    measurementId: "G-13S9Z6SMB4"
+};
